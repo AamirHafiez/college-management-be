20ac93724d197127125db8cd2b90fcfc42f0250e
Aamir <aamirhafiez@gmail.com>
2021-01-10 03:40:02 +0530

    added comments for better readability

--- controllers/api/v1/studentsController.js
+++ controllers/api/v1/studentsController.js
@@ -6,25 +6,32 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 
+// to sign up student
 module.exports.createStudent = async (req, res) => {
     try {
+        // finding student in user database
         let student = await User.findOne({email: req.body.email});
-        if(student) {
+        if(student) { // if existes send message as json
             return res.json({
                 'message': 'user already exists',
             });
         }
-        if(req.body.year === ''){
+        if(req.body.year === ''){ // server validation for students year
             throw "student year not valid";
         }
-        let hashedPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword;
-        req.body['userType'] = 'student';
-        await User.create(req.body);
-        return res.json({
+        if(req.body.password !== req.body.verify_password){ // check if password and verify pass are same
+            return res.json({
+                'message': 'password/verify password do not match'
+            });
+        }
+        let hashedPassword = await bcrypt.hash(req.body.password, 10); // hashing password using bcrypt
+        req.body.password = hashedPassword; // putting hashed password in the body
+        req.body['userType'] = 'student'; // putting user type in body
+        await User.create(req.body); // creating new user in database using whole body
+        return res.json({ // send success message as json
             'message': 'success'
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'internal server error',
@@ -32,29 +39,34 @@ module.exports.createStudent = async (req, res) => {
     }
 }
 
+// to sigin student
 module.exports.studentLogin = async (req, res) => {
     try {
+        // finding student in the user database
         let student = await User.findOne({email: req.body.email});
+        // if user not found or user is a type teacher send message student not found
         if(!student || student.userType === 'teacher'){
             res.json({
                 'message': 'student not found'
             });
             return;
         }
-        let checkPassword = await bcrypt.compare(req.body.password, student.password);
-        if(!checkPassword){
+        // if above conditions are false comapare password from body with that in DB
+        // as password in db is a hashed password use bcrypt to compare
+        let checkPassword = await bcrypt.compare(req.body.password, student.password); 
+        if(!checkPassword){ // if password is wrong send below json as message
             res.json({
                 'message': 'email/password invalid'
             });
             return;
         }
-        return res.json({
-            'message' : 'student authenticated',
-            'data': {
+        return res.json({ // if everything goes correct authenticate user and send a jwt token with expiry 
+            'message' : 'student authenticated', // of 24 hours, this token will be stored in cache of client
+            'data': { // every request client makes it will be authenticated using this token using passport js
                 'token': jwt.sign(student.toJSON(), 'collegeManagement', {expiresIn: 1000 * 60 * 60 * 24})
             }
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'internal server error',
@@ -62,26 +74,29 @@ module.exports.studentLogin = async (req, res) => {
     }
 }
 
+// to update details of student
 module.exports.updateStudentDetails = async (req, res) => {
     try {
-        if(req.body.password !== req.body.verify_password){
+        if(req.body.password !== req.body.verify_password){  // check if password and verify pass are same
             return res.json({
                 'message': 'password/verify password do not match'
             });
         }
-        if(req.body.password == ''){
+        if(req.body.password == ''){ // if password is nothing then just add the same password that was before
             req.body.password = req.user.password
         }
-        if(req.body.email) {
+        let hashedPassword = await bcrypt.hash(req.body.password, 10); // hashing password using bcrypt
+        req.body.password = hashedPassword; // putting hashed password in the body
+        if(req.body.email) { // if somehow user ties to update the email this gives status unauthorized 
             return res.status(401).json({
                 'message': 'Server error'
             });
         }
-        await User.findByIdAndUpdate(req.user._id, req.body);
+        await User.findByIdAndUpdate(req.user._id, req.body); // if all conditions follow then update
         return res.json({
             'message': 'updated successfully'
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'internal server error',
@@ -89,47 +104,54 @@ module.exports.updateStudentDetails = async (req, res) => {
     }
 }
 
+// to get all upcoming assignments
 module.exports.getUpcomingAssignments = async (req, res) => {
     try {
+        // finding all the assignments based on the year and populating with teacher that gave them
         let assignments = await Assignment.find({year: req.user.year}).populate('teacher');
+        // finding the user in the database and populating the assignmentsSubmitted
         let user = await User.findById(req.user._id).populate('assignmentsSubmitted');
-        let { assignmentsSubmitted } = user;
-        let indexesToDelete = [];
+        let { assignmentsSubmitted } = user; // only getting the assignmentsSubmitted
+        let indexesToDelete = []; 
         let assignmentIdsToDelete = {};
 
+        // mapping submitted assignments to get what assignments have been submimitted and storing them
+        // in an object like a hashmap to delete for O(1) time complexity afterwards
         assignmentsSubmitted.map((assignment) => {
             assignmentIdsToDelete[assignment._id] = true;
         });
 
         let count = 0;
-
+        // getting todays date
         let todaysDate = new Date();
+
+        // getiing the indexes of assignments that need to be deleted using the map
         assignments.map((assignment) => {
-            if(assignmentIdsToDelete[assignment._id]){
-                indexesToDelete.push(count);
+            if(assignmentIdsToDelete[assignment._id]){ // if assignment present in map
+                indexesToDelete.push(count); // then push the index (count) 
             }
-            if(assignment.deadline <= todaysDate){
-                indexesToDelete.push(count);
+            if(assignment.deadline <= todaysDate){ // if assignment deadline over
+                indexesToDelete.push(count); // push the index
             }
             count++;
         });
 
-        indexesToDelete.map((index) => {
+        indexesToDelete.map((index) => { // replaceing the indexed to be deleted to empty objects
             assignments.splice(index, 1, {});
         });
 
-        let assignmentResponse = [];
-        assignments.map((i) => {
-            if(i._id){
+        let assignmentResponse = []; // initialize reponse of assignments
+        assignments.map((i) => { // map through assignments having empty object if deleted
+            if(i._id){ // if the assignment has id (i.e, it is not an empty object) push in response list
                 assignmentResponse.push(i);
             }           
         });
 
-        return res.json({
+        return res.json({ // send response
             message: 'Assignments',
             assignments: assignmentResponse
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'internal server error',
@@ -137,70 +159,79 @@ module.exports.getUpcomingAssignments = async (req, res) => {
     }
 }
 
+
+// upload an assignment
 module.exports.uploadAssignment = async (req, res) => {
     try {
-        let user = await User.findById(req.user._id);
-        let assignment = await Assignment.findById(req.headers.id);
-        User.uploadedFile(req, res, (error) => {
-            if (error) {
+        let user = await User.findById(req.user._id); //  find user in the database
+        let assignment = await Assignment.findById(req.headers.id); // find assignment by getting id from headers
+        User.uploadedFile(req, res, (error) => { // using multer function in the user schema to uplad on server
+            if (error) { // if error send server error
                 return res.status(500).json(error);
             }
-            if(req.file) {
+            if(req.file) { 
+                // else push the uploaded assignment path to the submitted assignment paths of user schema
                 user.assignmentsSubmittedPaths.push(path.join(assignment._id + '/' + __dirname + '/../../../uploads/users/files') + '/' + req.file.filename);
-                user.assignmentsSubmitted.push(req.headers.id);
+                user.assignmentsSubmitted.push(req.headers.id); // also push what assignment has been submitted
+                // in the user schema
             }
-            user.save();
+            user.save(); // save user db
         });
-        assignment.submittedBy.push(req.user._id);
-        assignment.save();
+        assignment.submittedBy.push(req.user._id); // push the user that submitted the assignment in assignment schema
+        assignment.save(); // save assignment schema
 
-        return res.json({
+        return res.json({ // return message uploaded
             'message': 'assignment uploaded'
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         return res.status(500).json(error);
     }
 }
 
+// get all the submitted assignments
 module.exports.getSubmittedAssignments = async (req, res) => {
     try {
+        // find the user and populate assignmentsSubmitted and the teacher of all submitted assignments
         let user = await User.findById(req.user._id).populate({
             path: 'assignmentsSubmitted',
             populate: {
                 path: 'teacher'
             }
         });
+        // get the list of submitted assignments
         let assignmentsSubmitted = user.assignmentsSubmitted;
-        if(assignmentsSubmitted.length <= 0){
+        if(assignmentsSubmitted.length <= 0){ // if the list size is 0 send no assignment submitted
             return res.json({
                 'assignmentsSubmitted': 'none'
             });
         }
-        return res.json({
+        return res.json({ // else send submitted assignments
             'assignmentsSubmitted': assignmentsSubmitted
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         return res.status(500).json(error);
     }
 }
 
+// to get grade of a particular assignment
 module.exports.getGrade = async (req, res) => {
     try {
+        // find the grade in grade schema using query having student id and assignment id
         let grade = await Grade.find({
             'student' : req.user._id,
             'assignment': req.query.assignment
         });
-    
+        // if grade is found
         if(grade.length === 1){
-            return res.json({
+            return res.json({ // send the grade
                 'grade': grade[0].grade
             });
-        }else{
+        }else{ // if not
             return res.json({
-                'grade': ''
+                'grade': '' // send empty string to denote not graded yet
             });
         }
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log(error);
         return res.status(500).json(error);
     }
--- controllers/api/v1/teacherController.js
+++ controllers/api/v1/teacherController.js
@@ -6,56 +6,68 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 
+// signing up the teacher
 module.exports.createTeacher = async (req, res) => {
     try {
+        // find the teacher in the user database
         let teacher = await User.findOne({email: req.body.email})
-        if(teacher) {
+        if(teacher) { // if teacher already exists send message teacher exists
             return res.json({
                 'message': 'user already exists',
             });
-        }
-        if(req.body.subject === ''){
+        } 
+        if(req.body.subject === ''){ // if subject does not exist send subject not valid
             throw "techer subject not valid"
         }
+        if(req.body.password !== req.body.verify_password){ // check if password and verify pass are same
+            return res.json({
+                'message': 'password/verify password do not match'
+            });
+        }
+        // hash the password before saving it to the db
         let hashedPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword;
-        req.body['userType'] = 'teacher';
-        await User.create(req.body);
+        req.body.password = hashedPassword; // put hashed password to the body
+        req.body['userType'] = 'teacher'; // put new key in body as user type as teacher
+        await User.create(req.body); // create the user in the db using the body
         return res.json({
             'message': 'success'
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
-        return res.status(500).json({
+        return res.status(500).json({ 
             'message': 'internal server error',
         });
     }
 }
 
+// signing in the teacher
 module.exports.teacherLogin = async (req, res) => {
     try {
+        // find the teacher in user db isong email
         let teacher = await User.findOne({email: req.body.email});
+        // if teacher is not found or if found but user type is student 
         if(!teacher || teacher.userType === 'student'){
-            res.json({
+            res.json({ // send teacher not found
                 'message': 'teacher not found'
             });
             return;
         }
-        
+        // if above conditions are false comapare password from body with that in DB
+        // as password in db is a hashed password use bcrypt to compare
         let checkPassword = await bcrypt.compare(req.body.password, teacher.password);
-        if(!checkPassword){
+        if(!checkPassword){ // if password is wrong send below json as message
             res.json({
                 'message': 'email/password invalid'
             });
             return;
         }
-        return res.json({
-            'message' : 'teacher authenticated',
-            'data': {
+        return res.json({ // if everything goes correct authenticate user and send a jwt token with expiry 
+            'message' : 'teacher authenticated', // of 24 hours, this token will be stored in cache of client
+            'data': { // every request client makes it will be authenticated using this token using passport js
                 'token': jwt.sign(teacher.toJSON(), 'collegeManagement', {expiresIn: 1000 * 60 * 60 * 24})
             }
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'internal server error',
@@ -63,26 +75,29 @@ module.exports.teacherLogin = async (req, res) => {
     }
 }
 
+// to update teacher details
 module.exports.updateTeacherDetails = async (req, res) => {
     try {
-        if(req.body.password !== req.body.verify_password){
+        if(req.body.password !== req.body.verify_password){ // check if password and verify pass are same
             return res.json({
                 'message': 'password/verify password do not match'
             });
         }
-        if(req.body.password == ''){
+        if(req.body.password == ''){ // if password is nothing then just add the same password that was before
             req.body.password = req.user.password
         }
-        if(req.body.email) {
+        let hashedPassword = await bcrypt.hash(req.body.password, 10); // hashing password using bcrypt
+        req.body.password = hashedPassword; // putting hashed password in the body
+        if(req.body.email) { // if somehow user ties to update the email this gives status unauthorized 
             return res.status(401).json({
                 'message': 'Server error'
             });
         }
-        await User.findByIdAndUpdate(req.user._id, req.body);
+        await User.findByIdAndUpdate(req.user._id, req.body);// if all conditions follow then update
         return res.json({
             'message': 'updated successfully'
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'Server error'
@@ -90,6 +105,7 @@ module.exports.updateTeacherDetails = async (req, res) => {
     }
 }
 
+// adding a new assignment
 module.exports.addAssignment = async (req, res) => {
     let {
         title,
@@ -98,20 +114,23 @@ module.exports.addAssignment = async (req, res) => {
         deadline
     } = req.body;
 
+    // check if titile, description, year and deadline are not empty
     if(title === '' || description === '' || year === '' || deadline === ''){
-        return res.status(406).json({
+        return res.status(406).json({ // if true send not accepted
             'message': 'not acceptable'
         });
     }
 
+    // create new key in body as teacher and set it as the id of teacher that is logged in
     req.body['teacher'] = req.user._id;
 
     try {
-        await Assignment.create(req.body);
-        return res.json({
+        // create a new assignment using the body
+        await Assignment.create(req.body); 
+        return res.json({ // send reply as added
             'message': 'assignment added'
         });   
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log('error: ', error);
         return res.status(500).json({
             'message': 'internal server error',
@@ -119,38 +138,46 @@ module.exports.addAssignment = async (req, res) => {
     }
 }
 
+// to get the assignments given by the teacher
 module.exports.getAssignments = async (req, res) => {
-
     try {
+        // find assignments using the teacher id
         let assignments =await Assignment.find({teacher: req.user._id});
-        return res.json({
+        return res.json({ // send the assignments
             'assignments': assignments
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         return res.status(500).json(error);
     }
 }
 
+// to view the submissions 
 module.exports.viewSubmissions = async (req, res) => {
     try {
+        // get the assignment if from the query
         let assignmentId = req.query.id;
+        // find the assignment in assignment schema and populate submitted by
         let assignment = await Assignment.findById(assignmentId).populate('submittedBy');
-
+        // set a response list that will have all the submissions
         let response = [];
 
-        for(let i =0 ; i < assignment.submittedBy.length; i++){
-            
+        // traverse all the submitted by list
+        for(let i = 0 ; i < assignment.submittedBy.length; i++){
+            // get the grade for all the submitted assignments which will be send with the response
+            // so that teacher knows which assignment has been graded and what the grade is
+            // find the grade using assignment id and student id
             let studentGrade = await Grade.find({
                 'student': assignment.submittedBy[i]._id,
                 'assignment': assignment._id
             });
 
             let grade = '', isGraded = false;
-            if(studentGrade.length === 1){
-                grade = studentGrade[0].grade;
-                isGraded = true;
+            if(studentGrade.length === 1){ // if the assignment has been graded of a student
+                grade = studentGrade[0].grade; // get the grade of student
+                isGraded = true; // put a key true that specifies that assignment has been graded
             }
 
+            // push an object to response list having all the below details
             response.push({
                 '_id': assignment.submittedBy[i]._id,
                 'name': assignment.submittedBy[i].name,
@@ -160,49 +187,57 @@ module.exports.viewSubmissions = async (req, res) => {
                 'isGraded': isGraded
             });
         }
-
-        res.json({
+        res.json({ // send the response list
             'submittedBy': response
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         console.log(error)
         return res.status(500).json(error);
     }
 }
 
+// to add a grade to assignment of a student
 module.exports.addGrade = async (req, res) => {
     try {
+        // check if grade is found using student id and assignment id
         let isGraded = await Grade.find({
             'student': req.body.student,
             'assignment': req.body.assignment
         });
-        if(isGraded.length > 0){
-            console.log('already present');
-            return res.status(409).json({'message': 'already graded'});
+        if(isGraded.length > 0){ // if grade is found
+            return res.status(409).json({'message': 'already graded'}); // send response (conflict) already graded
         }
-        await Grade.create(req.body);
-        res.json({
+        await Grade.create(req.body); // add grade to the body
+        res.json({ // send message that grade has been added
             'message': 'grade added'
         });
-    } catch (error) {
+    } catch (error) { // if error send server error message
         return res.status(500).json(error);
     }
 }
 
+// download the pdf of the a particular student and assignment
 module.exports.downloadPDF = async (req, res) => {
     try {
+        // find the user in the db
         let user = await User.findById(req.query.student);
+        // get the paths submitted
         let assignmentsSubmittedPaths = user.assignmentsSubmittedPaths;
         let fileName;
         for(let i = 0; i < assignmentsSubmittedPaths.length; i++){
+            // slice the paths to get assignment ids
             let slicedPaths = assignmentsSubmittedPaths[i].split('/');
+            // if the assignment id from req matches to that the loop is traversing
+            // assignment id is will be the 0 th index of the spit 
             if(assignmentsSubmittedPaths[i].split('/')[0] === req.query.assignment){
+                // get fileName from as the filename is the last 
                 fileName = slicedPaths[slicedPaths.length - 1];
             }
         }
+        // get the file path by joining the paths below
         let filePath = path.join(__dirname, '../../../uploads/users/files/', fileName);
-        return res.download(filePath);
-    } catch (error) {
+        return res.download(filePath); // send response to download as a blob file
+    } catch (error) { // if error send server error message
         console.log('error', error);
         return res.status(500).json(error);
     }
